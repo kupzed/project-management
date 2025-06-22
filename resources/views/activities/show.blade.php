570bad500ec9dc70af8475b386a12a9d5a253b9f
kupzed <rizafahdansyahda3@gmail.com>
2025-06-22 22:41:24 +0700

    fix project and activity details

--- resources/views/activities/show.blade.php
+++ resources/views/activities/show.blade.php
@@ -17,6 +17,12 @@
                 </h2>
                 <div class="mt-1 flex flex-col sm:flex-row sm:flex-wrap sm:mt-0 sm:space-x-6">
                     <div class="mt-2 flex items-center text-sm text-gray-500">
+                        <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
+                        </svg>
+                        Aktivitas: {{ $activity->due_date->format('d F Y') }}
+                    </div>
+                    <div class="mt-2 flex items-center text-sm text-gray-500">
                         <span class="inline-flex rounded-full px-2 text-xs font-semibold leading-5 
                             @if($activity->status === 'completed') bg-emerald-100 text-emerald-800 
                             @elseif($activity->status === 'pending') bg-rose-100 text-rose-800
@@ -25,16 +31,10 @@
                             @endif "> {{ $activity->status }}
                         </span>
                     </div>
-                    <div class="mt-2 flex items-center text-sm text-gray-500">
-                        <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
-                        </svg>
-                        Aktivitas: {{ $activity->due_date->format('d F Y') }}
-                    </div>
                 </div>
             </div>
-            <div class="mt-4 flex md:mt-0 md:ml-4">
-                <a href="{{ route('activities.edit', $activity) }}" class="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <div class="mt-2 flex md:mt-0 md:ml-4">
+                <a href="{{ route('activities.edit', $activity) }}" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     Edit Aktivitas
                 </a>
             </div>
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -16,6 +16,12 @@
                 </h2>
                 <div class="mt-1 flex flex-col sm:flex-row sm:flex-wrap sm:mt-0 sm:space-x-6">
                     <div class="mt-2 flex items-center text-sm text-gray-500">
+                        <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
+                        </svg>
+                        Mulai: {{ $project->start_date->format('d F Y') }}
+                    </div>
+                    <div class="mt-2 flex items-center text-sm text-gray-500">
                         <span class="inline-flex rounded-full px-2 text-xs font-semibold leading-5 
                             @if($project->status === 'Complete') bg-green-100 text-green-800
                             @elseif($project->status === 'Ongoing') bg-blue-100 text-blue-800
@@ -25,16 +31,10 @@
                             @endif"> {{ $project->status }}
                         </span>
                     </div>
-                    <div class="mt-2 flex items-center text-sm text-gray-500">
-                        <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
-                        </svg>
-                        Mulai: {{ $project->start_date->format('d F Y') }}
-                    </div>
                 </div>
             </div>
-            <div class="mt-4 flex md:mt-0 md:ml-4">
-                <a href="{{ route('projects.edit', $project) }}" class="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <div class="mt-2 flex md:mt-0 md:ml-4">
+                <a href="{{ route('projects.edit', $project) }}" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     Edit Project
                 </a>
             </div>
@@ -55,7 +55,7 @@
                     <div class="bg-white-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                         <dt class="text-sm font-medium text-gray-500">Status</dt>
                         <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                            <span class="inline-flex rounded-full px-2 text-xs font-semibold leading-5 
+                            <span class="inline-flex rounded-full text-xs font-semibold leading-5 
                                 @if($project->status === 'Complete') bg-green-100 text-green-800
                                 @elseif($project->status === 'Ongoing') bg-blue-100 text-blue-800
                                 @elseif($project->status === 'Prospect') bg-yellow-100 text-yellow-800
