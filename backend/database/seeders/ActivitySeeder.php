7d629a326f6faa0e5eec6d84a0d9c2e352765490
Kupzed <rizafahdansyahda3@gmail.com>
2026-04-13 15:57:05 +0700

    feat(refactor): database seeders

--- backend/database/seeders/ActivitySeeder.php
+++ backend/database/seeders/ActivitySeeder.php
@@ -3,14 +3,12 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Project;
 use App\Models\Activity;
-use App\Models\Mitra;
 
 class ActivitySeeder extends Seeder
 {
     public function run(): void
     {
-        \App\Models\Activity::factory(1032)->create();
+        Activity::factory(1032)->create();
     }
 } 
